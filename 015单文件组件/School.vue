12278148c6c360f6cb35847c7c5abd88320a582b
sinkingwen <1132986394@qq.com>
2023-07-08 20:53:54 +0800

    20230708

--- 015单文件组件/School.vue
+++ 015单文件组件/School.vue
@@ -1,13 +1,39 @@
 <template>
-  
+  <div>
+    <h2>学校名称：{{ schoolName }}</h2>
+    <h2>校龄：{{ schoolAge }}</h2>
+    <button @click="showMotto">点击弹出校训</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'School',
+  data() {
+    return {
+      schoolName: 'NKU',
+      schoolAge: 104,
+      schoolMotto: '允公允能，日新月异'
+    }
+  },
+  methods: {
+    showMotto() {
+      alert(this.schoolMotto)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+h2 {
+  font-family: serif;
+  color: blueviolet;
+}
+button {
+  width: 20px;
+  height: 10px;
+  background-color: #fff;
+  font-size: 16px;
+  font-family: serif;
+}
 </style>
---
+++ 015单文件组件/Student.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="stu">
+    <h2>学生姓名：{{ stuName }}</h2>
+    <h2>学生性别：{{ stuGender }}</h2>
+    <h2>学生年龄：{{ stuAge }}</h2>
+    <h2>出生地：{{ from }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      stuName: '卢卢',
+      stuAge: 18,
+      stuGender: '男',
+      from:'福建省'
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.stu{
+  background-color: orange;
+}
+</style>
